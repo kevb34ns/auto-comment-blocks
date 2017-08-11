d2e8a9282740dd9501109ac36df970b1c793b23d
Kevin Yang <yangkevi@usc.edu>
2017-08-11 15:24:37 -0700

    onEnterRules and break using shift+enter added, not working yet

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-import { languages, commands, workspace, ExtensionContext, IndentAction, LanguageConfiguration } from 'vscode';
+import { languages, commands, workspace, ExtensionContext, IndentAction, LanguageConfiguration, OnEnterRule } from 'vscode';
 
 export function activate(context: ExtensionContext) {
     
@@ -8,19 +8,31 @@ export function activate(context: ExtensionContext) {
         .get<Array<string>>(Configuration.languagesSetting);
 
     langArray.forEach((lang) => {
-        console.log(lang);
-        let disposable = languages.setLanguageConfiguration(
-            lang, Configuration.languageConfiguration);
+        let config= Configuration.languageConfiguration;
+        if ((lang === 'c' || lang === 'cpp') &&
+                Configuration.getConfiguration().get<Boolean>(Configuration.singleLineBlocksSetting)) {
+
+            config.onEnterRules = config.onEnterRules.concat(Configuration.singleLineBlockEnterRules);
+        }
+
+        let disposable = languages.setLanguageConfiguration(lang, config);
+
         context.subscriptions.push(disposable);
     });
 
     commands.registerTextEditorCommand("auto-comment-blocks.break-single-line-block", (textEditor, edit, args) => {
-        //TODO check if line is // or ///, and replace the line with an empty line; handle multi cursor(?) 
-        //TODO need to add onCommand activation event to prevent annoying command not found warnings
-        if (textEditor.document.languageId === 'c' || 
-                textEditor.document.languageId === 'cpp') {
+        if (Configuration.getConfiguration().get<Boolean>
+                (Configuration.singleLineBlocksSetting) &&
+                (textEditor.document.languageId === 'c' || 
+                textEditor.document.languageId === 'cpp') &&
+                textEditor.selection.isEmpty) {
             
-            if (textEditor.selection.isEmpty) {
+            let line = textEditor.document.lineAt(textEditor.selection.active);
+
+            if (line.text.search(/^\s*\/\/\/\s*/) !== -1 ||
+                line.text.search(/^\s*\/\/\s*/) !== -1) {
+                
+        //      TODO handle indentation on newline
                 edit.insert(textEditor.selection.active, "\n");
             }
         }
@@ -75,6 +87,19 @@ class Configuration {
             }
         ]
     }
+    
+
+    //TODO '//' regex has to check that there are no more '/'s following it
+    static singleLineBlockEnterRules: Array<OnEnterRule> = [
+        {
+            beforeText: /^\s*\/\//,
+            action: { indentAction: IndentAction.None, appendText: '// '}
+        },
+        {
+            beforeText: /^\s*\/\/\//,
+            action: { indentAction: IndentAction.None, appendText: '/// '}
+        }
+    ]
 
     static getConfiguration() {
 
