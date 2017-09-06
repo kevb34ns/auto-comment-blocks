7db529085045b1328b822f9761e060aac8380113
Kevin Yang <yangkevi@usc.edu>
2017-09-05 21:38:34 -0700

    begin refactoring the code, currently non-functional

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,18 +1,22 @@
 'use strict';
 
-import { IndentAction, LanguageConfiguration, OnEnterRule, workspace } from 'vscode';
+import { Disposable, ExtensionContext, IndentAction, LanguageConfiguration, OnEnterRule, commands, languages, workspace, } from 'vscode';
+
+let fs = require('fs');
 
 export class Configuration {
   
-  static extensionName: string = "auto-comment-blocks";
-
-  static languagesSetting: string = "languages";
-
-  static singleLineBlocksSetting: string = "singleLineBlocksEnabled";
-
-  static breakSingleLineBlockCommand: string = "auto-comment-blocks.breakSingleLineBlock";
+  private readonly extensionName: string = "auto-comment-blocks";
+  private readonly languagesSetting: string = "languages";
+  private readonly singleLineBlocksSetting: string = "singleLineBlocksEnabled";
+  private readonly breakSingleLineBlockCommand: string = 
+      "auto-comment-blocks.breakSingleLineBlock";
+  private readonly singleLineConfigFile: string = __dirname +
+      "/../../language-configuration/single-line-configuration.json";
+  private readonly multiLineConfigFile: string = __dirname +
+      "/../../language-configuration/multi-line-configuration.json";
   
-  static languageConfiguration: LanguageConfiguration = {
+  private languageConfiguration: LanguageConfiguration = {
 
     onEnterRules: [
       {
@@ -50,7 +54,8 @@ export class Configuration {
     ]
   }
     
-  static singleLineBlockEnterRules: Array<OnEnterRule> = [
+  // TODO remove this
+  private singleLineBlockEnterRules: Array<OnEnterRule> = [
     {
       // e.g. // ...|
       beforeText: /^\s*\/\/(?!\/)/,
@@ -63,8 +68,56 @@ export class Configuration {
     }
   ]
 
-  static getConfiguration() {
+  private singleLineBlocksMap: Map<string, string> = new Map();
 
-    return workspace.getConfiguration(Configuration.extensionName);
+  private getConfiguration() {
+
+    return workspace.getConfiguration(this.extensionName);
+  }
+
+  private getMultiLineLanguages(): Array<string> {
+
+    let multiLineConfig = JSON.parse(fs.readFileSync(
+        this.multiLineConfigFile, 'utf-8'));
+    return multiLineConfig["languages"];
+  }
+
+  private setMultiLineConfiguration(langId: string) {
+
+    let config = this.languageConfiguration;
+    return languages.setLanguageConfiguration(langId, config);
+  }
+
+  private configureMultiLineBlocks(context: ExtensionContext) {
+    
+    let langIdArray = this.getMultiLineLanguages();
+    langIdArray.forEach((langId) => {
+      let disposable = this.setMultiLineConfiguration(langId);
+      context.subscriptions.push(disposable);
+    })
+  }
+
+  private configureSingleLineBlocks() {
+
+    let singleLineConfig: Object = JSON.parse(fs.readFileSync(
+      this.singleLineConfigFile, 'utf-8'));
+    let commentStyles = Object.keys(singleLineConfig);
+    for (let key in commentStyles) {
+      for (let langId in singleLineConfig[key]) {
+        this.singleLineBlocksMap.set(langId, key);
+      }
+  
+      // TODO get user-customized langIds for this key and add to the map
+    }
+  }
+
+  configureCommentBlocks(context: ExtensionContext) {
+
+    this.configureMultiLineBlocks(context);
+    this.configureSingleLineBlocks();
+  }
+
+  registerCommands() {
+    // TODO create new insert single line block command, check that the languageId is in the map (and not on the blacklist), and insert the mapped value into the document
   }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -3,79 +3,67 @@
 import { languages, commands, workspace, ExtensionContext, IndentAction, LanguageConfiguration, OnEnterRule, Disposable } from 'vscode';
 import { Configuration } from './configuration';
 
-var fs = require('fs');
+let fs = require('fs');
+let configuration = new Configuration();
 
 export function activate(context: ExtensionContext) {
 	
-	// set language configurations for multi-line Javadoc type blocks
-	// TODO first get the disabledLanguages array and check each langId against it before setting single or multi-line blocks
-  let languageIdArray = getLanguageIds();
-	languageIdArray.forEach((languageId) => {
-
-    let disposable = setLanguageConfiguration(languageId);
-		context.subscriptions.push(disposable);
-	});
+	configuration.configureCommentBlocks(context);
+	configuration.registerCommands();
 	
-	// TODO create map from singleLineConfig and make it accessible to the registered single-line block command
-	let singleLineFile: string = __dirname + 
-		'/../../language-configuration/single-line-configuration.json';
-	let singleLineConfig: Object = JSON.parse(fs.readFileSync(
-		singleLineFile, 'utf-8'));
-	let commentStyles = Object.keys(singleLineConfig);
-	for (let key in commentStyles) {
-		for (let langId in singleLineConfig[key]) {
-			// TODO add entry <langId, key> to map
-		}
+	// TODO first get the disabledLanguages array and check each langId against it before setting single or multi-line blocks
+  // let languageIdArray = getLanguageIds();
+	// languageIdArray.forEach((languageId) => {
 
-		// TODO get user-customized langIds for this key and add to the map
-	}
+  //   let disposable = setLanguageConfiguration(languageId);
+	// 	context.subscriptions.push(disposable);
+	// });
 
-	// register single-line block command 
-	registerCommands();
+	// // register single-line block command 
+	// registerCommands();
 }
 
 export function deactivate() {
   
 }
 
-// TODO should now be getting this from the multi-line config file
-function getLanguageIds(): Array<string> {
-	return Configuration.getConfiguration()
-	    .get<Array<string>>(Configuration.languagesSetting);
-}
+// function getLanguageIds(): Array<string> {
+// 	return Configuration.getConfiguration()
+// 	    .get<Array<string>>(Configuration.languagesSetting);
+// }
 
-function setLanguageConfiguration(languageId: string): Disposable {
-	let config: LanguageConfiguration = { };
-  if ((languageId === 'c' || languageId === 'cpp') &&
-      Configuration.getConfiguration()
-          .get<Boolean>(Configuration.singleLineBlocksSetting)) {
+// function setLanguageConfiguration(languageId: string): Disposable {
+// 	let config: LanguageConfiguration = { };
+//   if ((languageId === 'c' || languageId === 'cpp') &&
+//       Configuration.getConfiguration()
+//           .get<Boolean>(Configuration.singleLineBlocksSetting)) {
 
-		config.onEnterRules = Configuration.languageConfiguration.onEnterRules
-						.concat(Configuration.singleLineBlockEnterRules);
-	} else {
-		config.onEnterRules = Configuration.languageConfiguration.onEnterRules;
-	}
+// 		config.onEnterRules = Configuration.languageConfiguration.onEnterRules
+// 						.concat(Configuration.singleLineBlockEnterRules);
+// 	} else {
+// 		config.onEnterRules = Configuration.languageConfiguration.onEnterRules;
+// 	}
 
-  return languages.setLanguageConfiguration(languageId, config);
-}
+//   return languages.setLanguageConfiguration(languageId, config);
+// }
 
-function registerCommands() {
-  commands.registerTextEditorCommand(Configuration.breakSingleLineBlockCommand, 
-      (textEditor, edit, args) => {
+// function registerCommands() {
+//   commands.registerTextEditorCommand(Configuration.breakSingleLineBlockCommand, 
+//       (textEditor, edit, args) => {
 
-		if (Configuration.getConfiguration().get<Boolean>
-				(Configuration.singleLineBlocksSetting) &&
-				(textEditor.document.languageId === 'c' || 
-				textEditor.document.languageId === 'cpp') &&
-				textEditor.selection.isEmpty) {
+// 		if (Configuration.getConfiguration().get<Boolean>
+// 				(Configuration.singleLineBlocksSetting) &&
+// 				(textEditor.document.languageId === 'c' || 
+// 				textEditor.document.languageId === 'cpp') &&
+// 				textEditor.selection.isEmpty) {
 			
-			let line = textEditor.document.lineAt(textEditor.selection.active);
-			if (line.text.search(/^\s*\/\/\/\s*/) !== -1 ||
-				line.text.search(/^\s*\/\/\s*/) !== -1) {
+// 			let line = textEditor.document.lineAt(textEditor.selection.active);
+// 			if (line.text.search(/^\s*\/\/\/\s*/) !== -1 ||
+// 				line.text.search(/^\s*\/\/\s*/) !== -1) {
 				
-				let indentedNewline = '\n' + line.text.substring(0, line.text.search(/\//));
-				edit.insert(textEditor.selection.active, indentedNewline);
-			}
-		}
-	});
-}
+// 				let indentedNewline = '\n' + line.text.substring(0, line.text.search(/\//));
+// 				edit.insert(textEditor.selection.active, indentedNewline);
+// 			}
+// 		}
+// 	});
+// }
