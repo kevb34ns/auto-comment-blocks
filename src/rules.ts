987f6d4d3847dfb5ed262791674654c84ed263ff
Kevin Yang <yangkevi@usc.edu>
2017-09-13 20:40:04 -0700

    add rules class with onenterrule objects

---
+++ src/rules.ts
@@ -0,0 +1,70 @@
+'use strict';
+
+import { IndentAction, OnEnterRule } from 'vscode';
+
+export class Rules {
+
+  static readonly multilineEnterRules: OnEnterRule[] = [
+    {
+      // e.g. /** | */
+      beforeText: /^\s*\/\*\*(?!\/)([^\*]|\*(?!\/))*$/,
+      afterText: /^\s*\*\/$/,
+      action: { indentAction: IndentAction.IndentOutdent, appendText: ' * ' }
+    }, {
+      // e.g. /** ...|
+      beforeText: /^\s*\/\*\*(?!\/)([^\*]|\*(?!\/))*$/,
+      action: { indentAction: IndentAction.None, appendText: ' * ' }
+    }, {
+      // e.g. /*! | */
+      beforeText: /^\s*\/\*\!(?!\/)([^\*]|\*(?!\/))*$/,
+      afterText: /^\s*\*\/$/,
+      action: { indentAction: IndentAction.IndentOutdent, appendText: ' * ' }
+    }, {
+      // e.g. /*! ...|
+      beforeText: /^\s*\/\*\!(?!\/)([^\*]|\*(?!\/))*$/,
+      action: { indentAction: IndentAction.None, appendText: ' * ' }
+    }, {
+      // e.g.  * ...|
+      beforeText: /^(\t|(\ ))*\ \*(\ ([^\*]|\*(?!\/))*)?$/,
+      action: { indentAction: IndentAction.None, appendText: '* ' }
+    }, {
+      // e.g.  */|
+      beforeText: /^(\t|(\ ))*\ \*\/\s*$/,
+      action: { indentAction: IndentAction.None, removeText: 1 }
+    },
+    {
+      // e.g.  *-----*/|
+      beforeText: /^(\t|(\ ))*\ \*[^/]*\*\/\s*$/,
+      action: { indentAction: IndentAction.None, removeText: 1 }
+    }
+  ]
+
+  static readonly slashEnterRules: OnEnterRule[] = [
+    {
+      // e.g. // ...|
+      beforeText: /^\s*\/\/(?!\/)/,
+      action: { indentAction: IndentAction.None, appendText: '// '}
+    },
+    {
+      // e.g. /// ...|
+      beforeText: /^\s*\/\/\//,
+      action: { indentAction: IndentAction.None, appendText: '/// '}
+    }
+  ]
+
+  static readonly hashEnterRules: OnEnterRule[] = [
+    {
+      // e.g. # ...|
+      beforeText: /^\s*#(?!\/)/,
+      action: { indentAction: IndentAction.None, appendText: '# '}
+    }
+  ]
+
+  static readonly semicolonEnterRules: OnEnterRule[] = [
+    {
+      // e.g. ; ...|
+      beforeText: /^\s*;(?!\/)/,
+      action: { indentAction: IndentAction.None, appendText: '; '}
+    }
+  ]
+}
